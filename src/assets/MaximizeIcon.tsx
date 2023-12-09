6edc60ed3c593b7a069cd2be07525442d0b794a3
dev-pengi <siffffdoul@gmail.com>
2023-12-09 11:59:34 +0100

    style: add min and max icons

---
+++ src/assets/MaximizeIcon.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+
+const MaximizeIcon: FC = () => {
+  return (
+    <svg
+      fill="currentColor"
+      width="100%"
+      viewBox="0 0 32 32"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1.25 1.25v29.5h29.5v-29.5zM29.25 29.25h-26.5v-26.5h26.5z"></path>
+    </svg>
+  );
+};
+
+export default MaximizeIcon;
---
+++ src/assets/MinimizeIcon.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+
+const MinimizeIcon: FC = () => {
+  return (
+    <svg
+      width="100%"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3 12L21 12"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default MinimizeIcon;
